2e4fa2fbb9b063bc7c4a927aee30d0fa2b64ef5f
MadhunikaAkula <57282785+MadhunikaAkula@users.noreply.github.com>
2025-03-19 15:54:31 +0530

    component creation

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {
+  menuOpen: boolean = false;
+
+  // Toggle the hamburger menu
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Close menu after clicking a link
+  closeMenu() {
+    this.menuOpen = false;
+  }
+}
